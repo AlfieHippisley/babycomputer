7ed6077a663f29b565027cf2898ba587fe583f39
Alfie Hippisley <alfiehippisley10@gmail.com>
2018-11-16 23:00:47 +0000

    Read File Binary And Place In Store

--- manchester.cpp
+++ manchester.cpp
@@ -6,25 +6,25 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
-
+// We need this unless you want to type std:: :)
 using namespace std;
 
 // If this is true, main menu will keep displaying
 bool runProgram = true;
 
-int store[];
+// We use a 32 bit wide & deep store
+int bitSize = 32;
 
-// MainMenu() - Purpose :
-// This displays the menu to the user so they can choose an option
+// A 2D array in c is an array of pointers to arrays
+int** store = new int*[bitSize];
+
+// initializeStore() - Purpose :
+// This will create a 2d array that will act as the store
+// Note For Me Later (Alfie) - Get the StackOverflow used for reference
 
 void initializeStore() {
-	
-	// We use a 32 bit wide & deep store
-	int bitSize = 32;
-
-	// A 2D array in c is an array of pointers to arrays
-	int** store = new int*[bitSize];
 
 	// We go down the array of pointers creating new arrays of 32 bits
 	for (int i = 0; i < bitSize; ++i) 
@@ -42,6 +42,57 @@ void initializeStore() {
 	}
 }
 
+// fillStore() - Purpose :
+// Fill the store with predefined binary code from a file
+// Note For Me - University Of Wollongong used for reference
+
+void fillStore() {
+
+	// This will store the lines as they come in
+	string line;
+
+	// This is the file link
+	ifstream myfile("BabyTest1-MC.txt");
+
+	// This keeps count of what line we are on	 
+	int counter = 0;
+
+	// If the file has been found and is open
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line)) // While there are still lines in the file
+		{
+			for (int i = 0; i < bitSize; i++) // For each index in the string up to defined bitsize eg 32 (String is array of char)
+			{	
+				char lineChar = line[i]; // The file reads in as Strings, if we use them we would end up storing ascii values
+				store[counter][i] = lineChar -'0'; // So we convert the ascii to ints for storage and processing
+			}
+
+			// Thats a line complete, so increase counter by one manually due to while loop
+			counter++;
+		}
+
+		// Close file as we no longer need it
+		myfile.close();
+	}
+
+	// If we cant find the file display this error message
+	else cout << "Unable to open file";
+
+	// Print the values in the array (Debugging only, remove before submission)
+	for (int i = 0; i < bitSize; i++)
+	{
+		cout << endl << i << " Line Is - " << endl;
+
+		for (int i2 = 0; i2 < bitSize; i2++)
+		{
+			cout << store[i][i2];
+		}
+	}
+}
+
+// mainMenu() - Purpose :
+// This displays the user menu and takes their inputs
 void mainMenu() {
 	
 	// Create Int To Store User Input
@@ -63,6 +114,7 @@ void mainMenu() {
 		// Case 1 - Run Default Binary Code
 		case 1:
 			initializeStore();
+			fillStore();
 		break;
 
 		// Case 2 - Import Binary Code
